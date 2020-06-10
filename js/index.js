bf89f494d324361aff296b5bf29f0bd22d8c8fb0
Roman Andriiv <roman.andriyiv@gmail.com>
2020-06-10 13:50:12 +0200

    carousel section and slider add

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-alert(1);
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 700,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/prew.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/next.png"></button>',
+        responsive: [
+            {
+                 breakpoint: 768,
+                 settings: {
+                    dots: true,
+                    arrows: false
+                 }
+             }
+         ]
+    });
+});
