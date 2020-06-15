f321ae3f2afea1f5263521754dc2f7faedd36689
Roman Andriiv <roman.andriyiv@gmail.com>
2020-06-15 11:55:37 +0200

    slider add to index.js

--- js/index.js
+++ js/index.js
@@ -2,16 +2,14 @@ $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 700,
         // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/prew.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/next.png"></button>',
-        responsive: [
-            {
-                 breakpoint: 768,
-                 settings: {
-                    dots: true,
-                    arrows: false
-                 }
-             }
-         ]
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prew.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/next.png"></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
     });
 });
