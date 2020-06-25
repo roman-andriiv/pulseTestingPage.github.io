1a8eaffa2845bb52db2d22bdf0ebd64b8c4b7416
Roman <roman.andriyiv@gmal.com>
2020-06-25 17:27:42 +0200

    modal__close  and some edding add

--- js/index.js
+++ js/index.js
@@ -36,24 +36,4 @@ $(document).ready(function () {
     toggleSlide('.catalog-item__back');
 
 
-
-    // icon color script from "img" to "svg". For changing icon color
-
-    $('img.img-svg').each(function () {
-        var $img = $(this);
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
-        $.get(imgURL, function (data) {
-            var $svg = $(data).find('svg');
-            if (typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass + ' replaced-svg');
-            }
-            $svg = $svg.removeAttr('xmlns:a');
-            if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-            }
-            $img.replaceWith($svg);
-        }, 'xml');
-    });
-
 });
