0811a2989e19d1af1d598a1b88cc9a248fea6111
Roman <roman.andriyiv@gmal.com>
2020-06-26 18:10:07 +0200

    modal finished, add modal windows open-close script

--- js/index.js
+++ js/index.js
@@ -36,4 +36,21 @@ $(document).ready(function () {
     toggleSlide('.catalog-item__back');
 
 
+    // Modal windows
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('fast');
+    });
+
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('fast');
+    });
+
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('fast');
+        })
+    });
+
+
 });
